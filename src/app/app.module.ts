5228ac7e2846ecf0b99603890bd3ea7071308133
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-05-10 17:56:42 -0600

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPageComponent,
+    HeaderComponent,
+    FormComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
